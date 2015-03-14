e77da773277314a2a6cc6248f30f1a00181c555b
Yang <zhy0216@gmail.com>
2015-03-14 12:52:33 +0800

    make basic backbone stuff

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 $(function(){
 
+    var Spinner = Backbone.Model.extend({
+        defaults: {
+
+            type: null, // start from "1"
+            width: "30px",
+            height: "30px",
+            backgroundColor: "#333",
+
+        }
+
+    })
+
+    var spinnerCollection = new Backbone.Collection([
+
+        ], {
+            model: Spinner,
+        })
+
+
+
+    var SpinnerFormView = Backbone.View.extend({
+
+
+
+    })
+
+    var SpinnerShowView = Backbone.View.extend({
+
+    })
+
+
 
 
 
