cef66bb90c15d5627bd4ed87f736b54e9ff617e8
Yang <zhy0216@gmail.com>
2015-03-14 14:18:33 +0800

    make backbone routr

--- index.js
+++ index.js
@@ -1,36 +1,62 @@
 $(function(){
 
+// util func
 
 // backbone stuff
     var Spinner = Backbone.Model.extend({
-        defaults: {
 
-            type: null, // start from "1"
+        makeUpUrl: function(){
+            var urlTemplate = _.template("make/type=<%= type %>&width=<%= width %>&height=<%= height %>&bgc=<%= backgroundColor %>");
+            return urlTemplate(this.attributes);
+        },
+
+        defaults: {
+            type: "1", // start from "1"
             width: "30px",
             height: "30px",
-            backgroundColor: "#333",
-
+            backgroundColor: "rgba(25,1,1,0.27)",
         }
-
     })
 
-    var spinnerCollection = new Backbone.Collection([
+    var SPINNER = new Spinner();
 
-        ], {
-            model: Spinner,
-        })
+    var SPINNER_FORM_VIEW = new (Backbone.View.extend({
 
 
 
-    var SpinnerFormView = Backbone.View.extend({
-
-
-
-    })
+    }))({el: $("#spinner-form"), model: SPINNER});
 
     var SpinnerShowView = Backbone.View.extend({
 
-    })
+    });
+
+    var ROUTER = new (Backbone.Router.extend({
+              routes: {
+                '': 'make',
+                'show-code/:spinnertype': 'showCode',
+                'make/type=:type&width=:width&height=:height&bgc=:bgc': 'make',
+              },
+
+              make: function(type, width, height, bgc){
+                    if(type){SPINNER.type = type;}
+                    if(width){SPINNER.width = width;}
+                    if(height){SPINNER.height = height;}
+                    if(bgc){SPINNER.backgroundColor = bgc;}
+
+                    if(!type || !width || !height || !bgc){
+                        this.navigate(SPINNER.makeUpUrl());
+                    }
+
+              }
+
+
+    }))();
+
+
+
+
+
+    
 
 
 
@@ -42,7 +68,7 @@ $('.color-picker').colorpicker({
 });
 
 
-
+Backbone.history.start();
 
 
 })
